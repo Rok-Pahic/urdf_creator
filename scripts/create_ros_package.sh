7f87bc1a0e3ba1483f006de7b556ba5acc19b523
rpahic <rok.pahic@marovt.com>
2022-05-20 14:38:50 +0200

    added ros create

--- scripts/create_ros_package.sh
+++ scripts/create_ros_package.sh
@@ -1,11 +1,10 @@
 #! /bin/sh
-
-echo Chose part name:
-read part_name
+ros_ws_path=$2
+part_name=$1
 package_tail="_rospkg"
 package_name="${part_name}${package_tail}"
 echo $package_name
-cd /ros_ws/src
+cd $ros_ws_path
 catkin_create_pkg $package_name
 cd $package_name
-mkdir urdf
+mkdir geometry
